e9d9d98b20e6e3a21046181cbe6e829a61452592
SantiagoRS <santiago.reyess@campusucc.edu.co>
2024-09-24 17:54:04 -0500

    Se creo dto de user y usercontroller

--- src/main/java/login/demo/login/controller/UserController.java
+++ src/main/java/login/demo/login/controller/UserController.java
@@ -1,5 +1,23 @@
 package login.demo.login.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all")
+    public List<User> getAll {
+        return userService.getAll();
+    }
     
 }
--- src/main/java/login/demo/login/domain/dto/User.java
+++ src/main/java/login/demo/login/domain/dto/User.java
@@ -1,5 +1,26 @@
 package login.demo.login.domain.dto;
 
-public class User {
-    
+public class User{
+
+    private int idAuthor;
+    private String username;
+    private String role;
+
+
+public int getIdAuthor() {
+    return idAuthor;
+}
+public void setIdAuthor(int idAuthor) {
+    this.idAuthor = idAuthor;
+}
+public String getUsername() {
+    return username;
+}
+public void setUsername(String name) {
+    this.username = username;
+}
+public String getRole() {
+    return role;
+}
+
 }

3752898e5cb19c2633516e7b368bb57d32fbd5da
SantiagoRS <santiago.reyess@campusucc.edu.co>
2024-09-24 18:01:32 -0500

    Se creo user controller/repository/service

--- src/main/java/login/demo/login/controller/UserController.java
+++ src/main/java/login/demo/login/controller/UserController.java
@@ -4,6 +4,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import login.demo.login.domain.dto.User;
+import login.demo.login.domain.service.UserService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 

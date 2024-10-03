fc439707de9a540b4366148915a5263bf6ba5421
Cardenasucc <deivid.cardenas@campusucc.edu.co>
2024-10-03 17:59:43 -0500

    carpetas service controller repository

---
+++ src/main/java/login/demo/login/controller/User.controller.java
@@ -0,0 +1,22 @@
+package login.demo.login.controller;
+
+import java.util.List;
+
+import org.mapstruct.ap.shaded.freemarker.core.ReturnInstruction.Return;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/Users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all")
+    public  List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
